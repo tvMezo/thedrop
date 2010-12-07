f0bc05641ca939f518adc0c948773ef8641bdd89
Ted Veatch <tveatch@mezolink.com>
2010-12-06 20:28:31 -0600

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thedrop::Application.config.secret_token = 'b229be1f0d207d34f1fc5e92c4888fa7b9172c96820312d750c43c3d82117ab4043f0720bf5ac18d2be7461371d6368325329afcd46ad473f33b771b9eb59f3c'
